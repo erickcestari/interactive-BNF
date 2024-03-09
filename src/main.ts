4f35e26b6d5b98304f3eaa522d959c62f79a9c15
Erick Cestari <erickcestari03@gmail.com>
2024-03-09 16:55:18 -0300

    feat: remove useless params to interaction bnfs

--- src/main.ts
+++ src/main.ts
@@ -30,15 +30,11 @@ const initial = getInitialNet() ?? `
 
 const inputTextarea = document.getElementById("input") as HTMLTextAreaElement
 const outputPre = document.getElementById("output")!
-const parallel = document.getElementById("parallel") as HTMLInputElement
-const autoSubst = document.getElementById("autoSubst") as HTMLInputElement
 
 inputTextarea.value = initial
 
 inputTextarea.addEventListener("change", exec)
 inputTextarea.addEventListener("keyup", exec)
-parallel.addEventListener("change", exec)
-autoSubst.addEventListener("change", exec)
 
 exec()
 
@@ -48,20 +44,9 @@ function exec() {
     const net = parseNet(inputTextarea.value)
     let steps = 0
     for (; steps < 1000; steps++) {
-      if (autoSubst.checked) {
-        while (net[1].some((x) => x[0].type === "aux" || x[1].type === "aux")) {
-          net[1] = net[1].flatMap((x) =>
-            x[0].type === "aux" || x[1].type === "aux" ? reduce(x) : [x]
-          )
-        }
-      }
       output += concatHoriz(drawNet(net), [...printNet(net), ""]).join("\n") + "\n\n"
       if (!net[1].length) break
-      if (parallel.checked) {
-        net[1] = net[1].flatMap(reduce)
-      } else {
-        net[1] = reduce(net[1][0]!).concat(net[1].slice(1))
-      }
+      net[1] = reduce(net[1][0]!).concat(net[1].slice(1))
     }
 
     outputPre.textContent = `${steps} steps\n\n` + output
